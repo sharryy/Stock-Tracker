fde9c7d1f8178682cb7ea4bd2268878479d664ff
Sharryy <ibneadam388@gmail.com>
2021-10-26 01:11:59 +0500

    Found Myster

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,15 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    /** @test */
+    public function it_checks_stocks_for_products_at_retailers()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+        $this->assertFalse($switch->inStock());
     }
 }
