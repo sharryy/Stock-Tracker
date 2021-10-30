df976d7f5e4bdc479daf4a35ccc10ee7ee51b30b
Sharryy <ibneadam388@gmail.com>
2021-10-30 18:59:16 +0500

    Even More Verbose

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit;
 
-use App\Clients\Client;
 use App\Clients\ClientException;
 use App\Clients\StockStatus;
 use App\Models\Retailer;
@@ -10,7 +9,6 @@ use App\Models\Stock;
 use Database\Seeders\RetailerWithProductSeeder;
 use Facades\App\Clients\ClientFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Mockery;
 use Tests\TestCase;
 
 class StockTest extends TestCase
@@ -30,11 +28,7 @@ class StockTest extends TestCase
     function it_updates_local_stock_status_after_being_tracked()
     {
         $this->seed(RetailerWithProductSeeder::class);
-
-        $clientMock = Mockery::mock(Client::class);
-        $clientMock->shouldReceive('checkAvailability')->andReturn(new StockStatus(true, 9900));
-
-        ClientFactory::shouldReceive('make')->andReturn($clientMock);
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(new StockStatus(true, 9900));
         $stock = tap(Stock::first())->track();
         $this->assertTrue($stock->in_stock);
         $this->assertEquals(9900, $stock->price);
