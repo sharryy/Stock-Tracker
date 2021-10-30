0863f24baf42ce6b63a3042964f86cac3fcc8c4a
Sharryy <ibneadam388@gmail.com>
2021-10-30 12:15:33 +0500

    Create Seeder Class

--- tests/Feature/TrackCommandTest.php
+++ tests/Feature/TrackCommandTest.php
@@ -16,19 +16,6 @@ class TrackCommandTest extends TestCase
     /** @test */
     function it_tracks_product_stock()
     {
-        $switch = Product::create(['name' => 'Nintendo Switch']);
-        $bestBuy = Retailer::create(['name' => 'Best Buy']);
-        $this->assertFalse($switch->inStock());
-
-        $stock = new Stock([
-            'price' => 100,
-            'url' => 'https://www.foo.com',
-            'sku' => 12345,
-            'in_stock' => false
-        ]);
-
-        $bestBuy->addStock($switch, $stock);
-        $this->assertFalse($stock->fresh()->in_stock);
         Http::fake(function () {
             return [
                 'available' => true,
