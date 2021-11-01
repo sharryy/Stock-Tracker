4e8bbe79fb0e742ee62a5ccead738a342946c8aa
Sharryy <ibneadam388@gmail.com>
2021-11-01 20:30:55 +0500

    Notification Test Started

--- tests/Feature/TrackCommandTest.php
+++ tests/Feature/TrackCommandTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Clients\StockStatus;
 use App\Models\Product;
+use App\Models\User;
+use App\Notifications\ImportantStockUpdate;
 use Database\Seeders\RetailerWithProductSeeder;
-use Http;
+use Facades\App\Clients\ClientFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Notification;
 use Tests\TestCase;
 
 class TrackCommandTest extends TestCase
@@ -17,20 +21,27 @@ class TrackCommandTest extends TestCase
     {
         $this->seed(RetailerWithProductSeeder::class);
 
-//        dd(Product::all());
-
         /*
          * Only for Unit Testing
          */
 
         $this->assertFalse(Product::first()->inStock());
-        Http::fake(function () {
-            return [
-                'available' => true,
-                'price' => 12900
-            ];
-        });
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(new StockStatus($available = true, $price = 29900));
+
         $this->artisan('track')->expectsOutput("All Done");
         $this->assertTrue(Product::first()->inStock());
     }
+
+    /** @test */
+    function it_notifies_the_user_when_the_stock_changes_in_a_notable_way()
+    {
+        Notification::fake();
+        $user = User::factory()->create(['email' => 'sharryy@gmail.com']);
+        $this->seed(RetailerWithProductSeeder::class);
+        ClientFactory::shouldReceive('make->checkAvailability')->andReturn(new StockStatus($available = true, $price = 29900));
+        $this->artisan('track');
+        Notification::assertSentTo($user, ImportantStockUpdate::class);
+
+    }
+
 }
